8f602b70d560804d37919732d6a655258e379d91
Rebeca <rehdomingos123@gmail.com>
2024-10-15 12:02:54 -0300

    Remoção temporária do script de avaliação

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -2,7 +2,6 @@ import Login from "../models/login.js";
 import createHashWithSalt from "../middleware/hashWithSalt.js";
 import Admin from "../models/admin.js";
 import { ERROR_MESSAGE } from "../constants/Messages.js";
-import '../scripts/distributeDraws.js'
 import transporter from '../middleware/emailConfig.js';
 import crypto from 'crypto';
 
@@ -106,59 +105,6 @@ class AdminController{
         catch (err) {
             res.status(400).json({ message: err.message });
           }
-    }
-
-    startEvaluator = async (req, res, next) => {
-        response = this.distributeDraws();
-
-        return res.status
-    }
-
-
-    distributeDraws = async() => {
-        try {
-            // Obter todos os desenhos e avaliadores do banco de dados
-            const draws = await Draw.find();
-            const evaluators = await Evaluator.find();
-    
-            // Verifique se há avaliadores suficientes
-            if (evaluators.length === 0) {
-                console.log("Não há avaliadores disponíveis.");
-                return false;
-            }
-    
-            // Distribuir os desenhos de forma aleatória
-            for (const draw of draws) {
-                // Obter dois avaliadores aleatórios
-                const randomEvaluators = [];
-                while (randomEvaluators.length < 2) {
-                    const randomIndex = Math.floor(Math.random() * evaluators.length);
-                    const evaluator = evaluators[randomIndex];
-                    if (!randomEvaluators.includes(evaluator._id)) {
-                        randomEvaluators.push(evaluator._id);
-                    }
-                }
-    
-                // Adicionar os avaliadores ao desenho
-                draw.review = randomEvaluators.map(evaluatorId => ({
-                    evaluator: evaluatorId,
-                    numberOfAlertsEvaluator: 0, // ou outro valor inicial
-                    score: null,
-                    note: null,
-                    date: null,
-                    finished: false,
-                }));
-    
-                // Salvar as alterações no desenho
-                await draw.save();
-            }
-    
-            console.log("Distribuição de desenhos concluída.");
-            return true;
-        } catch (error) {
-            console.error("Erro ao distribuir desenhos:", error);
-            return false;
-        }
     }
 
     generateRandomPassword(length) {
